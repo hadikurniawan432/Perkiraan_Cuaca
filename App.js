c3a0d1cafbbe1e6d443dfc87b4ccecab84fa583c
hadikurniawan432 <37114443+hadikurniawan432@users.noreply.github.com>
2018-03-26 18:13:20 +0700

    Perkiraan_Cuaca

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AppWeather1615051003 from './src/comp/AppWeather1615051003';
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <AppWeather1615051003 />
+    );
+  }
+}
